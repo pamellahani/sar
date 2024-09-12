d9b6d5b828d6d77325eaeccb214c5a4edb78c5ef
Pamella Hani <pam02hani@gmail.com>
2024-09-12 18:00:36 +0200

    writing of use case

--- src/channels/tests/Test.java
+++ src/channels/tests/Test.java
@@ -1,5 +1,14 @@
 package channels.tests;
 
+/*
+ * USE CASE: 
+ * Multiple EchoClients (here 3) can connect to a single EchoServer, send data, and 
+ * receive the same data back (echoed by the server). 
+ * 
+ * The server listens for client connections and echoes any data it receives. Clients
+ * send a byte sequence and verify that the same sequence is echoed back by the server.
+ */
+
 public class Test {
 
     public static void main(String[] args) {
