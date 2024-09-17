8cefd9dc6c4f22a55d497c1b47ba03f63539de17
Pamella Hani <pam02hani@gmail.com>
2024-09-17 22:09:30 +0200

    code cleanup - WIP

--- src/channels/tests/EchoServerTest.java
+++ src/channels/tests/EchoServerTest.java
@@ -1,17 +1,7 @@
 package channels.tests;
 
-import channels.Broker;
-import channels.Channel;
-import channels.SimpleBroker;
-import channels.SimpleTask;
-import channels.Task;
-import channels.CircularBuffer;
-
 public class EchoServerTest {
     
-    private Broker serverBroker;
-    private Task serverTask;
-    private CircularBuffer buffer;
 
     /**
      * 
@@ -21,39 +11,7 @@ public class EchoServerTest {
      */
 
     public EchoServerTest(int port) {
-
-        this.buffer = new CircularBuffer(256);
-
-        // Create a new server broker and accept a connection on the specified port
-        this.serverBroker = new SimpleBroker("EchoServer");
-        Channel serverChannel = this.serverBroker.accept(port);
-
-        // Define the server task 
-        this.serverTask = new SimpleTask(serverBroker, () -> {
-            byte[] data = new byte[256];
-            while  (!serverChannel.disconnected()) {
-                int bytesRead = serverChannel.read(data, 0, data.length);
-                if (bytesRead > 0) {
-
-                    //Push data into the buffer
-                    for (int i = 0; i < bytesRead; i++) {
-                        buffer.push(data[i]);
-                    }
-
-                    //Pull data from the buffer to echo back to the client
-                    byte echoedData[] = new byte[bytesRead];
-                    for (int i = 0; i < bytesRead; i++) {
-                        echoedData[i] = buffer.pull();
-                    }
-
-                    //Write the data back to the client
-                    serverChannel.write(data, 0, bytesRead);
-                }
-            }
-        });
+        
     }
 
-    public void startServer() {
-        this.serverTask.start();
-    }
 }
