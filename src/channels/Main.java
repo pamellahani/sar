31a30dca3a9d8a5ec59622a89d7bfbda5d336196
Pamella Hani <pam02hani@gmail.com>
2024-09-22 21:44:00 +0200

    rewrite of main method

--- src/channels/Main.java
+++ src/channels/Main.java
@@ -3,8 +3,27 @@ package channels;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'main'");
+
+        BrokerManager manager = new BrokerManager();
+        Broker broker1 = new SimpleBroker("broker1", manager);
+        Broker broker2 = new SimpleBroker("broker2", manager);
+
+        Channel channel1 = broker1.accept(8080);
+        Channel channel2 = broker2.connect("broker1", 8080);
+
+        byte[] message = "Hello, World!".getBytes();
+        channel2.write(message, 0, message.length);
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = channel1.read(buffer, 0, buffer.length);
+        String received = new String(buffer, 0, bytesRead);
+        System.out.println(received);
+
+        channel1.disconnect();
+        channel2.disconnect();
     }
-    
+
+
 }
+    
+
