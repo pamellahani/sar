94e5314e8010ecfa5a0ada79731501379610e94a
Pamella Hani <pam02hani@gmail.com>
2024-09-25 23:02:24 +0200

    communication done

--- src/channels/Rdv.java
+++ src/channels/Rdv.java
@@ -8,37 +8,47 @@ public class Rdv {
 
     public synchronized Channel accept(Broker broker, int port) {
         this.brokerAcceptor = broker;
-        this.acceptingChannel = new SimpleChannel(brokerAcceptor, brokerConnector);  // Create the channel now that both brokers are present
-        
-        if (connectingChannel != null){
-            //brokerAcceptor.connect(brokerConnector.getName(), port); 
+        this.acceptingChannel = new SimpleChannel(brokerAcceptor, brokerConnector);  // Create the channel
+    
+        if (connectingChannel != null) {
             connectingChannel.connect(acceptingChannel, brokerAcceptor.getName());
+            notifyAll();  // Notify waiting connect call
+        } else {
+            waitForBroker(true);  // Wait for the connecting channel if not yet initialized
         }
-
+        
         return acceptingChannel;
     }
-
+    
     public synchronized Channel connect(Broker broker, int port) {
         this.brokerConnector = broker;
-        this.connectingChannel = new SimpleChannel(brokerAcceptor, brokerConnector);  // Create the channel now that both brokers are present
-
-        if (acceptingChannel != null){
-            //brokerAcceptor.connect(brokerConnector.getName(), port);
+        this.connectingChannel = new SimpleChannel(brokerAcceptor, brokerConnector);  // Create the channel
+    
+        if (acceptingChannel != null) {
             acceptingChannel.connect(connectingChannel, brokerConnector.getName());
-            notifyAll();
+            notifyAll();  // Notify waiting accept call
+        } else {
+            waitForBroker(false);  // Wait for the accepting channel if not yet initialized
         }
-        else{
-            waitForBroker();
-        }
+    
         return connectingChannel;
     }
+    
 
-    private synchronized void waitForBroker() {
-        while (acceptingChannel == null||connectingChannel == null) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
+    private synchronized void waitForBroker(boolean isAcceptor) {
+        try {
+            if (isAcceptor) {
+                while (acceptingChannel == null) {
+                    wait();  // Wait for the accepting channel to be initialized
+                }
+            } else {
+                while (connectingChannel == null) {
+                    wait();  // Wait for the connecting channel to be initialized
+                }
             }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
+    
 }
