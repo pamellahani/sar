b5a249e3b316251748cf700829c60e8f3f1f2207
Pamella Hani <pam02hani@gmail.com>
2024-09-26 11:09:57 +0200

    added print statement to main to follow flow

--- src/channels/Main.java
+++ src/channels/Main.java
@@ -20,9 +20,13 @@ public class Main {
                 Channel channel = serverBroker.accept(8080);
                 byte[] buffer = new byte[1024];
                 int bytesRead = channel.read(buffer, 0, buffer.length);
+                if (bytesRead == 0) {
+                    System.out.println("Server received no data.");
+                } else
                 if (bytesRead > 0) {
                     String receivedMessage = new String(buffer, 0, bytesRead);
                     System.out.println("Server received: " + receivedMessage);
+                    System.out.println("Server received bytes: " + bytesRead);
                 }
                 channel.disconnect();
             } catch (Exception e) {
@@ -38,8 +42,9 @@ public class Main {
                 System.out.println("Client connecting to server...");
                 Channel channel = clientBroker.connect("ServerBroker", 8080);
                 byte[] message = "Hello World".getBytes();
-                channel.write(message, 0, message.length);
+                int bytesSent = channel.write(message, 0, message.length);
                 System.out.println("Client sent: Hello World");
+                System.out.println("Client sent bytes: " + bytesSent);
                 channel.disconnect();
             } catch (Exception e) {
                 e.printStackTrace();
