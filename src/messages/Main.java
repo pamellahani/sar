6a83d0514d8ca731cc4cef444d0edc7a3cc78003
Pamella Hani <pam02hani@gmail.com>
2024-10-07 15:44:00 +0200

    refactoring

--- src/messages/Main.java
+++ src/messages/Main.java
@@ -12,35 +12,27 @@ public class Main {
         SimpleBroker clientBroker = new SimpleBroker("ClientBroker", brokerManager);
 
         // Initialize queue brokers for server and client using their respective brokers
-        SimpleQueueBroker serverQueueBroker = new SimpleQueueBroker(serverBroker);
-        SimpleQueueBroker clientQueueBroker = new SimpleQueueBroker(clientBroker);
+        QueueBrokerImpl serverQueueBroker = new QueueBrokerImpl(serverBroker);
+        QueueBrokerImpl clientQueueBroker = new QueueBrokerImpl(clientBroker);
 
 
         // Server task setup
-        MessageQueue serverQueue = serverQueueBroker.accept(5000);
+        MessageQueueImpl serverQueue = serverQueueBroker.accept(5000);
         System.out.println("Server queue created.");
-        MessageQueue clientQueue = clientQueueBroker.connect("ServerBroker", 5000);
+        MessageQueueImpl clientQueue = clientQueueBroker.connect("ServerBroker", 5000);
 
 
         Task server = new SimpleTask(serverBroker, () -> {
-            try {
-                System.out.println("Server is waiting for messages...");
-                byte[] received = serverQueue.receive();
-                System.out.println("Server received: " + new String(received));
-            } catch (DisconnectedException e) {
-                System.out.println("Server disconnected.");
-            }
+            System.out.println("Server is waiting for messages...");
+            byte[] received = serverQueue.receive();
+            System.out.println("Server received: " + new String(received));
         });
 
         // Client task setup
         Task client = new SimpleTask(clientBroker, () -> {
-            try {
-                String message = "Hello from client!";
-                clientQueue.send(message.getBytes(), 0, message.getBytes().length);
-                System.out.println("Client sent message.");
-            } catch (DisconnectedException e) {
-                System.out.println("Client disconnected.");
-            }
+            String message = "Hello from client!";
+            clientQueue.send(message.getBytes(), 0, message.getBytes().length);
+            System.out.println("Client sent message.");
         });
 
         // Start both tasks
