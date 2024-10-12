35055785cdfffe02fa8f43292739457d1e1313f5
Pamella Hani <pam02hani@gmail.com>
2024-10-12 16:34:40 +0200

    added accept listener for server

---
+++ src/hybrid/tests/EchoAcceptListener.java
@@ -0,0 +1,26 @@
+package hybrid.tests;
+
+import hybrid.MessageQueue;
+import hybrid.QueueBroker;
+import hybrid.tests.message_listeners.EchoServerMessageListener;
+
+public class EchoAcceptListener implements QueueBroker.AcceptListener {
+
+    private QueueBroker queue_broker;
+	private int client_counter = 0;
+	
+	 public EchoAcceptListener(QueueBroker qb) {
+		queue_broker = qb;
+	}
+
+	@Override
+	public void accepted(MessageQueue queue) {
+		
+		queue.setListener(new EchoServerMessageListener(queue));
+		
+		if(client_counter++ >= 2) {
+			queue_broker.unbind(80);
+		}
+	}
+}
+
