117fae4e74dfc6806d960cd6e417ace7e140f0d2
Pamella Hani <pam02hani@gmail.com>
2024-10-12 16:27:42 +0200

    updated ConnectListener to generate random messages
    to test

--- src/hybrid/tests/EchoConnectListener.java
+++ src/hybrid/tests/EchoConnectListener.java
@@ -1,27 +1,43 @@
 package hybrid.tests;
 
-import java.util.UUID;
-
 import hybrid.Message;
 import hybrid.MessageQueue;
 import hybrid.QueueBroker;
 import hybrid.tests.message_listeners.EchoClientMessageListener;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
 public class EchoConnectListener implements QueueBroker.ConnectListener {
 
-@Override
-	public void connected(MessageQueue messageQueue) {
-		
-		String msg = "Hello  Server";
-        Message message = new Message(msg.getBytes(), 0, msg.length());
-		
-		messageQueue.setListener(new EchoClientMessageListener(messageQueue, message));
-		
-		messageQueue.send(message);
-	}
+    @Override
+    public void connected(MessageQueue messageQueue) {
+        // Generate a random message instead of a static "Hello Server" message
+        String randomMessage = generateRandomMessage();
+        Message message = new Message(randomMessage.getBytes(StandardCharsets.UTF_8), 0, randomMessage.length());
 
-	@Override
-	public void refused() {
-		System.out.println("Server: Connection refused by the server.");
-	}
+        // Set the listener and send the random message
+        messageQueue.setListener(new EchoClientMessageListener(messageQueue, message));
+        messageQueue.send(message);
+    }
+
+    @Override
+    public void refused() {
+        System.out.println("Server: Connection refused by the server.");
+    }
+
+    // Method to generate a random string of alphabetic characters
+    private String generateRandomMessage() {
+        int length = 10; // Length of the random message
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder messageBuilder = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            messageBuilder.append(characters.charAt(random.nextInt(characters.length())));
+        }
+
+        return messageBuilder.toString();
+    }
 }
+
