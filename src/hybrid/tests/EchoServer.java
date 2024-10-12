ac6c029bff62f95e1465a789ec607466ad60da71
Pamella Hani <pam02hani@gmail.com>
2024-10-12 16:31:50 +0200

    updated EchoClient and EchoServer

--- src/hybrid/tests/EchoServer.java
+++ src/hybrid/tests/EchoServer.java
@@ -1,57 +1,19 @@
 package hybrid.tests;
 
-import channels.Task;
-import hybrid.*;
-import hybrid.Message;
-import hybrid.MessageQueue;
-import hybrid.QueueBroker; 
+import hybrid.QueueBroker;
 
-public class EchoServer {
-    private final int port;
-   // private final MixedQueueBroker broker;
-    private final EventTask serverTask; 
-
-    public EchoServer(int port) {
-        this.port = port;
-        this.serverTask = new EventTask("BrokerServer"); 
-        //this.broker = new MixedQueueBroker("EchoServer");
-    }
-
-    public void start() {
-        serverTask.post(() -> {
-            serverTask.queueBroker.bind(port, new QueueBroker.AcceptListener() {
-
-
-                @Override
-                public void accepted(MessageQueue queue) {
-                    System.out.println("Server: Client connected.");
-                    queue.setListener(new MessageQueue.Listener() {
-                        @Override
-                        public void received(byte[] msg) {
-                            String message = new String(msg);
-                            System.out.println("Server received: " + message);
-                            queue.send(new Message(msg, 0, msg.length));
-                        }
-
-                        @Override
-                        public void sent(Message msg) {
-                            System.out.println("Server: Echoed back the message.");
-                        }
-
-                        @Override
-                        public void closed() {
-                            System.out.println("Server: Client connection closed.");
-                            stop();
-                        }
-                    });
-                }
-            });
-        });
-        serverTask.start();  // Start the task's event processing
-    }
-
-    public void stop() {
-        System.out.println("Server: Shutting down.");
-        serverTask.kill(); 
-    }
+public class EchoServer implements Runnable{
+	
+    private QueueBroker _broker;
+        
+        public EchoServer(QueueBroker broker) {
+            _broker = broker;
+        }
+    
+    
+        @Override
+        public void run() {
+            _broker.bind(80, new EchoAcceptListener(_broker));
+            
+        }
 }
