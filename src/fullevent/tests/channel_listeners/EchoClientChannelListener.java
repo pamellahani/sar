5fec5a811deaecc36dc8b2419860d2b6d20e041a
Pamella Hani <pam02hani@gmail.com>
2024-10-19 21:20:37 +0200

    added EchoChannel classes

--- src/fullevent/tests/channel_listeners/EchoClientChannelListener.java
+++ src/fullevent/tests/channel_listeners/EchoClientChannelListener.java
@@ -1,5 +1,59 @@
 package fullevent.tests.channel_listeners;
 
-public class EchoClientChannelListener {
+import java.nio.charset.StandardCharsets;
+
+import channels.DisconnectedException;
+import fullevent.EventChannel;
+
+public class EchoClientChannelListener implements EventChannel.ChannelListener {
+
+
+    private final EventChannel channel;
+    private final String messageToSend;
+
+    public EchoClientChannelListener(EventChannel channel, String messageToSend) throws DisconnectedException {
+        this.channel = channel;
+        this.messageToSend  = messageToSend;
+        this.channel.setChannelListener(this);
+        sendInitialMessage();
+    }
+
+    private void sendInitialMessage() throws DisconnectedException {
+        byte[] messageBytes = messageToSend.getBytes(StandardCharsets.UTF_8);
+        channel.write(messageBytes, 0, messageBytes.length);
+        System.out.println("Client sent message: " + messageToSend);
+    }
+
+    @Override
+    public void onBufferFull(EventChannel channel) {
+        System.out.println("Client buffer is full, waiting to send more data.");
+    }
+
+    @Override
+    public void onBufferNotFull(EventChannel channel) {
+        System.out.println("Client buffer has space, can continue writing.");
+    }
+
+    @Override
+    public void onBufferEmpty(EventChannel channel) {
+        System.out.println("Client buffer is empty, waiting for new data.");
+    }
+
+    @Override
+    public void onBufferNotEmpty(EventChannel channel) {
+        byte[] buffer = new byte[256];
+        int bytesRead = -1;
+        try {
+            bytesRead = channel.read(buffer, 0, buffer.length);
+        } catch (DisconnectedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        if (bytesRead > 0) {
+            String receivedMessage = new String(buffer, 0, bytesRead, StandardCharsets.UTF_8);
+            System.out.println("Client received message: " + receivedMessage);
+        }
+    }
+    
     
 }
--- src/fullevent/tests/channel_listeners/EchoServerChannelListener.java
+++ src/fullevent/tests/channel_listeners/EchoServerChannelListener.java
@@ -1,5 +1,54 @@
 package fullevent.tests.channel_listeners;
 
-public class EchoServerChannelListener {
-    
+import channels.DisconnectedException;
+import fullevent.EventChannel;
+
+public class EchoServerChannelListener implements EventChannel.ChannelListener {
+
+    private final EventChannel channel;
+
+    public EchoServerChannelListener(EventChannel channel) {
+        this.channel = channel;
+        this.channel.setChannelListener(this);
+    }
+
+    @Override
+    public void onBufferFull(EventChannel channel) {
+        System.out.println("Server buffer is full, cannot accept more data at the moment.");
+    }
+
+    @Override
+    public void onBufferNotFull(EventChannel channel) {
+        System.out.println("Server buffer has space, can resume accepting data.");
+    }
+
+    @Override
+    public void onBufferEmpty(EventChannel channel) {
+        System.out.println("Server buffer is empty, waiting for client to send data.");
+    }
+
+    @Override
+    public void onBufferNotEmpty(EventChannel channel) {
+        byte[] buffer = new byte[256];
+        int bytesRead = -1;
+        try {
+            bytesRead = channel.read(buffer, 0, buffer.length);
+        } catch (DisconnectedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        if (bytesRead > 0) {
+            String message = new String(buffer, 0, bytesRead);
+            System.out.println("Server received message: " + message);
+            // Echo the message back to the client
+            try {
+                channel.write(buffer, 0, bytesRead);
+            } catch (DisconnectedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("Server echoed message back to client.");
+        }
+    }
+
 }
