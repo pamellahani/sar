b9b419c04ea7765da9b7af0e63086ed1e3ca886c
Pamella Hani <pam02hani@gmail.com>
2024-10-22 19:54:03 +0200

    added ShutdownEvent.java

---
+++ src/fullevent/ShutdownEvent.java
@@ -0,0 +1,18 @@
+package fullevent;
+
+/**
+ * Event to trigger the shutdown of the EventPump and the end of the program
+ */
+public class ShutdownEvent extends Event {
+
+    public ShutdownEvent() {
+        super(null, null, null);
+        System.out.println("ShutdownEvent created");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("ShutdownEvent triggered. Stopping EventPump.");
+        // No specific action needed, the existence of this event triggers the pump to stop
+    }
+}
