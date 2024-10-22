c5e416df384e6466486218a916dfbaa4b7500cb7
Pamella Hani <pam02hani@gmail.com>
2024-10-22 19:48:46 +0200

    updated EchoAcceptListener.java

--- src/fullevent/tests/EchoAcceptListener.java
+++ src/fullevent/tests/EchoAcceptListener.java
@@ -6,45 +6,29 @@ import fullevent.Channel;
 import fullevent.EventTask;
 import fullevent.tests.channel_listeners.EchoServerChannelListener;
 
+public class EchoAcceptListener implements AcceptListener {
 
+    @Override
+    public void accepted(Channel channel) {
+        channel.setChannelListener(new EchoServerChannelListener(channel));
+        
+        byte[][] byteArrays = { new byte[1024], new byte[1024], new byte[1024] };
+        
+        EventTask task = new EventTask();
+        
+        // Post read operations for each byte array
+        for (byte[] byteArray : byteArrays) {
+            task.post(() -> readFromChannel(channel, byteArray));
+        }
+    }
 
-public class EchoAcceptListener implements AcceptListener{
-
-	@Override
-	public void accepted(Channel channel) {
-		
-		channel.setChannelListener(new EchoServerChannelListener(channel));
-		
-		byte[] bytes1 = new byte[360];
-		byte[] bytes2 = new byte[360];
-		byte[] bytes3 = new byte[360];
-
-
-		
-		EventTask task = new EventTask();
-		task.post(() -> {
-			try {
-				channel.read(bytes1);
-			} catch (DisconnectedException e) {
-				e.printStackTrace();
-			}
-		});
-		task.post(() -> {
-			try {
-				channel.read(bytes2);
-			} catch (DisconnectedException e) {
-				e.printStackTrace();
-			}
-		});
-		task.post(() -> {
-			try {
-				channel.read(bytes3);
-			} catch (DisconnectedException e) {
-				e.printStackTrace();
-			}
-		});
-
-	}
-
+    // Helper method to read from the channel and handle exceptions
+    private void readFromChannel(Channel channel, byte[] byteArray) {
+        try {
+            channel.read(byteArray);
+            System.out.println("Server writing message back: " + new String(byteArray));
+        } catch (DisconnectedException e) {
+            e.printStackTrace();
+        }
+    }
 }
-	
