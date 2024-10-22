0623b86c2fa358acd81b9fbd95b0ea7d45c563be
Pamella Hani <pam02hani@gmail.com>
2024-10-22 19:51:22 +0200

    added Event.java
    same as hybrid package -> to ensure isolation between packages

---
+++ src/fullevent/Event.java
@@ -0,0 +1,23 @@
+package fullevent;
+
+public class Event implements Runnable{
+
+    private final EventTask currentTask;
+    private final Runnable runnable;
+
+    public Event(EventTask fromtask, EventTask mytask, Runnable r) {
+        currentTask = mytask;
+        runnable = r;
+        //System.out.println("New Event created for task: " + mytask.toString());
+    }
+
+    @Override
+    public void run() {
+        //System.out.println("Event running for task: " + currentTask);
+        this.runnable.run();
+    }
+
+    public EventTask getTask() {
+        return currentTask;
+    }
+}
