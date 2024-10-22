6654b9ef728831744200b85b95a640df5a7e1697
Pamella Hani <pam02hani@gmail.com>
2024-10-22 19:35:30 +0200

    updated main method

--- src/fullevent/tests/MultiClientTest.java
+++ src/fullevent/tests/MultiClientTest.java
@@ -1,5 +1,51 @@
 package fullevent.tests;
 
+import fullevent.EventBroker;
+import fullevent.EventPump;
+import fullevent.EventTask;
+
+
 public class MultiClientTest {
-    
+
+	private EchoClient clientRunnable;
+	private EchoServer serverRunnable;
+    private EventTask client1;
+	private EventTask client2;
+	private EventTask client3;
+	private EventTask server;
+	
+	
+	private void setup() {
+
+		EventBroker serverBroker = new EventBroker("serverBroker");
+	    EventBroker clientBroker = new EventBroker("clientBroker");
+		
+		clientRunnable = new EchoClient(clientBroker);
+		serverRunnable = new EchoServer(serverBroker);
+		
+	    server = new  EventTask();
+	    client1 = new EventTask();
+	    client2 = new EventTask();
+	    client3 = new EventTask();
+				
+	}
+	
+	public static void main(String[] args) {
+
+		MultiClientTest test = new MultiClientTest();
+		
+		test.setup();
+		
+		test.server.post(test.serverRunnable);
+	
+		test.client1.post(test.clientRunnable);
+		test.client2.post(test.clientRunnable);
+		test.client3.post(test.clientRunnable);
+	
+		EventPump.getInstance().run();
+		
+		System.out.println("TEST PASSED");
+	}
+	
+
 }
